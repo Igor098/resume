c4996300e7decd78ef34db6dca351b91eef1afe5
Igor098 <igor.novozhilov2018@gmail.com>
2024-11-12 23:50:23 +0300

    beauty remarks

--- js/index.js
+++ js/index.js
@@ -1,22 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Получение списка блоков для анимации
     let blocks = document.querySelectorAll('.block-animation');
 
-    function checkBlocksVisibility() {
-        let windowHeight = window.innerHeight;
+    /**
+     * Функция проверки видимости блока
+     */
+    const checkBlocksVisibility = () => {
+        let windowHeight = window.innerHeight; // Высота окна браузера
 
+        // Проверяем на видимость все блоки с классом анимации
         blocks.forEach(block => {
             let blockPosition = block.getBoundingClientRect().top;
 
+            //Если блок в активной области высоты с запасом 50px
             if (blockPosition < windowHeight - 50) {
-            block.style.opacity = "1";
-            block.style.transform = "translateY(0)";
-        }
-    });
+                block.style.opacity = "1";
+                block.style.transform = "translateY(0)";
+            }
+        });
     }
 
     checkBlocksVisibility();
 
-    window.addEventListener('scroll', function() {
+    // Обработчик для скролла страницы
+    window.addEventListener('scroll', () => {
         checkBlocksVisibility();
     });
 });
